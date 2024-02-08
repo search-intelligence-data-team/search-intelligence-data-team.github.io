7ee92b3dabc201c9caa4d03a5de3c691b52cfe02
ArekZajac-SI <157490443+ArekZajac-SI@users.noreply.github.com>
2024-02-08 09:49:51 +0000

    improved login security

--- login.js
+++ login.js
@@ -1,11 +1,10 @@
 function checkPassword() {
     var inputPassword = document.getElementById('password').value;
     var hashedInput = CryptoJS.SHA256(inputPassword).toString();
-
     var actualPasswordHash = '5ebac058d2c2204ebd554e965426e4dca6f6ae5be5b65d6aa5623b31026190fb';
 
     if (hashedInput === actualPasswordHash) {
-        // Redirect to the main page if the password is correct
+        document.cookie = "loggedIn=true; path=/";
         window.location.href = 'main.html';
     } else {
         alert('Incorrect password');
--- script.js
+++ script.js
@@ -51,3 +51,8 @@ function displayResults(cards, query) {
     });
 }
 
+function logout() {
+    document.cookie = "loggedIn=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    window.location.href = 'index.html';
+}
+
